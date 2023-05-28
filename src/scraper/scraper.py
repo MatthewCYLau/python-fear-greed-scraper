8c6770dd37ae40acc476b7b6feecded7d0f020f4
matthew.lau <matthew.lau@credera.co.uk>
2023-05-29 00:02:00 +0100

    feat: scaffolding for beautifulsoup

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,8 +1,15 @@
 import logging
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://realpython.github.io/fake-jobs/"
 class Scraper():
     def __init__ (self):
         pass;
 
     def start_scraper(self):
         logging.info('Start scraping...')
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id="ResultsContainer")
+        print(results.prettify())
