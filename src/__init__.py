d69b4729ab00abd78d7c03933b06e2e64361ea6f
matthew.lau <matthew.lau@credera.co.uk>
2023-06-02 17:47:52 +0100

    feat: updated cicd yaml to read mongo secret

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,13 @@ from dotenv import load_dotenv
 from src.scraper.scraper import Scraper
 from .db.setup import db_connect
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
-db_connect()
+if os.environ.get("MONGO_DB_CONNECTION_STRING"):
+    db_connect()
 
 def app():
     scraper = Scraper();
