1884f0fa6e723b07c1b6ab338987c3b2d408f0be
matthew.lau <matthew.lau@credera.co.uk>
2023-06-16 19:54:54 +0100

    feat: add logic to send emails by alert threshold

--- src/email/email.py
+++ src/email/email.py
@@ -1,11 +1,13 @@
 import sendgrid
 import os
+import logging
 from sendgrid.helpers.mail import Mail, Email, To, Content
 
-def send_email(index):
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
-    from_email = Email("lau.cy.matthew@gmail.com") 
-    to_email = To("lau.cy.matthew@gmail.com")
+
+def send_email(to_email: str = "", index: int = 0):
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get("SENDGRID_API_KEY"))
+    from_email = Email("lau.cy.matthew@gmail.com")
+    to_email = To(to_email)
     subject = "Python Fear and Greed Index notification"
     content = Content("text/plain", "Fear and greed index is: {}".format(index))
     mail = Mail(from_email, to_email, subject, content)
@@ -13,5 +15,5 @@ def send_email(index):
     mail_json = mail.get()
 
     response = sg.client.mail.send.post(request_body=mail_json)
-    print(response.status_code)
-    print(response.headers)
+    logging.info(response.status_code)
+    logging.info(response.headers)
--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,43 +1,60 @@
 import logging
 import time
 from selenium import webdriver
+from bson.objectid import ObjectId
 from src.email.email import send_email
+from src.db.setup import db
 from src.record.models import Record
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class Scraper():
-    def __init__ (self):
+
+class Scraper:
+    def __init__(self):
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--window-size=1920,1080")
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.implicitly_wait(10); 
+        self.driver.implicitly_wait(10)
 
     def start_scraper(self):
-        logging.info('Start scraping...')
+        logging.info("Start scraping...")
         try:
-            self.driver.get('https://edition.cnn.com/markets/fear-and-greed')
-            element_present = EC.visibility_of_any_elements_located((By.CLASS_NAME, 'market-fng-gauge__dial-number-value'))
+            self.driver.get("https://edition.cnn.com/markets/fear-and-greed")
+            element_present = EC.visibility_of_any_elements_located(
+                (By.CLASS_NAME, "market-fng-gauge__dial-number-value")
+            )
             WebDriverWait(self.driver, 5).until(element_present)
             time.sleep(5)
-            el = self.driver.find_element(By.CLASS_NAME, 'market-fng-gauge__dial-number-value')
+            el = self.driver.find_element(
+                By.CLASS_NAME, "market-fng-gauge__dial-number-value"
+            )
             index = el.text
-            logging.info('Fear and greed index is: %s', index)
+            logging.info("Fear and greed index is: %s", index)
             record = Record(index=index)
             record.save_to_database()
-            if (self.should_send_email(int(index))):
-                send_email(index)
-            else:
-                logging.info('Fear and greed index is above threshold')
+            alerts = self.get_alerts_equal_or_greater_than_current_index(int(index))
+            emails = [i["created_by"]["email"] for i in alerts]
+            [send_email(i, index) for i in emails]
+            logging.info("Email notification sent to %s emails", len(emails))
         except NoSuchElementException as ex:
             self.fail(ex.msg)
-    
+
     @staticmethod
     def should_send_email(index: int) -> bool:
         return index < 35
+
+    @staticmethod
+    def get_alerts_equal_or_greater_than_current_index(min_index: int):
+        alerts = list(db["alerts"].find({"index": {"$gt": min_index - 1}}).limit(0))
+        for alert in alerts:
+            if alert["created_by"]:
+                alert["created_by"] = db["users"].find_one(
+                    {"_id": ObjectId(alert["created_by"])}, {"password": False}
+                )
+        return alerts
