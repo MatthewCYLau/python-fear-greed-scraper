b91af23bf6408edb5c297f90328ff7b6be349701
matthew.lau <matthew.lau@credera.co.uk>
2023-07-15 16:49:35 +0100

    feat: add service account for cloud run runtime

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -8,3 +8,19 @@ resource "google_project_iam_member" "cloud_run_invoker" {
   role    = "roles/run.invoker"
   member  = "serviceAccount:${google_service_account.cloud_run_invoker.email}"
 }
+
+resource "google_service_account" "cloud_run_runtime" {
+  account_id   = "python-scraper-cr-runtime"
+  display_name = "Service Account for ${var.cloud-run-job-name} Cloud Run runtime"
+}
+
+resource "google_project_iam_member" "cloud_run_runtime" {
+  project = var.project
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.cloud_run_runtime.email}"
+  condition {
+    title       = "resource_name_equals_client_assets"
+    description = "Resource name equals client assets storage bucket"
+    expression  = "resource.name == '${google_storage_bucket.assets.name}'"
+  }
+}
