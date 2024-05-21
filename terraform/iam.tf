9a865b19a8d1ee00d37a98a59b02a96f0011054b
Matthew Lau <lau.cy.matthew@gmail.com>
2024-05-21 23:29:27 +0800

    chore: append project iam binding

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -46,7 +46,7 @@ resource "google_project_service_identity" "pubsub_agent" {
 resource "google_project_iam_binding" "project_token_creator" {
   project = var.project
   role    = "roles/iam.serviceAccountTokenCreator"
-  members = ["serviceAccount:${google_project_service_identity.pubsub_agent.email}"]
+  members = ["serviceAccount:${google_project_service_identity.pubsub_agent.email}", "user:lau.cy.matthew@gmail.com", ]
 }
 
 /* Use google_storage_bucket_iam_policy over google_project_iam_member */
