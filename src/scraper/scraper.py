8bc90e58714bbf8dc5820436d554cb711ed50137
Matthew Lau <lau.cy.matthew@gmail.com>
2024-12-24 07:46:02 +0000

    feat: email copy

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -92,7 +92,11 @@ class Scraper:
                 continue
             to_email_address = i["created_by"]["email"]
             note = i["note"]
-            message = f"Fear and greed index is: {index}\n{note}"
+            increase_amount = i["created_by"]["regularContributionAmount"] * (
+                1 + (index - i["index"] / 100)
+            )
+            increased_contribution = f"{increase_amount:.2f}"
+            message = f"Fear and greed index is: {index}\n{note}\nIncrease contribution by {increased_contribution}"
             send_email(to_email_address, message)
             emails_sent_count += 1
         logging.info("Email notification sent to %s emails", emails_sent_count)
