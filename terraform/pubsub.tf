467646f8240f20c3448e0d3635587e9ef0dd8284
Matthew Lau <lau.cy.matthew@gmail.com>
2025-11-14 11:50:07 +0000

    feat: orders topic and sub

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -57,3 +57,68 @@ resource "google_pubsub_schema" "analysis_job" {
     }
   )
 }
+
+resource "google_pubsub_topic" "orders" {
+  name = "orders-topic"
+  schema_settings {
+    schema   = "projects/${var.project}/schemas/${google_pubsub_schema.order.name}"
+    encoding = "JSON"
+  }
+  labels = local.labels
+}
+
+resource "google_pubsub_subscription" "order" {
+  name                       = "orders-subscription"
+  topic                      = google_pubsub_topic.orders.name
+  ack_deadline_seconds       = 20
+  message_retention_duration = "1200s" # 20 minutes
+  retain_acked_messages      = true
+
+  push_config {
+    push_endpoint = "${data.google_cloud_run_v2_service.api.uri}/api/orders-subscription-push"
+    oidc_token {
+      service_account_email = google_service_account.pubsub_invoker.email
+    }
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+  labels = local.labels
+}
+
+resource "google_pubsub_schema" "order" {
+  name = "order"
+  type = "AVRO"
+  definition = jsonencode({
+    "type" : "record",
+    "name" : "Avro",
+    "fields" : [
+      {
+        "name" : "user_id",
+        "type" : "string",
+        "default" : ""
+      },
+      {
+        "name" : "stock_symbol",
+        "type" : "string",
+        "default" : ""
+      },
+      {
+        "name" : "order_type",
+        "type" : "string",
+        "default" : ""
+      },
+      {
+        "name" : "quantity",
+        "type" : "int",
+        "default" : 0
+      },
+      {
+        "name" : "price",
+        "type" : "float",
+        "default" : 0
+      }
+    ]
+    }
+  )
+}
