c5d742c5d45a8554d19455118db2585ea04e12e9
Matthew Lau <lau.cy.matthew@gmail.com>
2026-01-12 04:29:17 +0800

    feat: iam refactor

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -87,8 +87,14 @@ resource "google_project_iam_member" "github_action_sa" {
   member  = "serviceAccount:github-actions-service-account@open-source-apps-001.iam.gserviceaccount.com"
 }
 
-resource "google_project_iam_member" "bigquery_admin" {
+resource "google_project_iam_member" "bigquery_job_user" {
   project = var.project
-  role    = "roles/bigquery.admin"
+  role    = "roles/bigquery.jobUser"
   member  = "serviceAccount:${google_service_account.cloud_run_runtime.email}"
+}
+
+resource "google_bigquery_dataset_iam_member" "dataset_editor" {
+  dataset_id = google_bigquery_dataset.records.dataset_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${google_service_account.cloud_run_runtime.email}"
 }
